a5243a4603822aa4d06ce8d9d0d01b23736881a5
swinejelly <sajordan121@gmail.com>
2013-11-10 10:47:36 -0500

    Initial Commit

---
+++ src/main/java/edu/rit/csh/PageTemplate.java
@@ -0,0 +1,15 @@
+package edu.rit.csh;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class PageTemplate extends WebPage {
+	private static final long serialVersionUID = -6071606580892913447L;
+
+	public PageTemplate(final PageParameters parameters){
+		add(new Label("header", "HEADER"));
+		add(new Label("body", "BODY"));
+		add(new Label("footer", "FOOTER"));
+	}
+}
---
+++ src/main/java/edu/rit/csh/WicketApplication.java
@@ -0,0 +1,32 @@
+package edu.rit.csh;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.protocol.http.WebApplication;
+
+/**
+ * Application object for your web application. If you want to run this application without deploying, run the Start class.
+ * 
+ * @see edu.rit.csh.Start#main(String[])
+ */
+public class WicketApplication extends WebApplication
+{    	
+	/**
+	 * @see org.apache.wicket.Application#getHomePage()
+	 */
+	@Override
+	public Class<? extends WebPage> getHomePage()
+	{
+		return PageTemplate.class;
+	}
+
+	/**
+	 * @see org.apache.wicket.Application#init()
+	 */
+	@Override
+	public void init()
+	{
+		super.init();
+
+		// add your configuration here
+	}
+}
