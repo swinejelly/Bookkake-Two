35a8603cc6e319c1efdce8bb1762cbb080cd7b16
swinejelly <sajordan121@gmail.com>
2013-12-15 19:56:10 -0500

    Restrict calendar range.

--- src/main/java/edu/rit/csh/OwnedBookSearchResultsPage.java
+++ src/main/java/edu/rit/csh/OwnedBookSearchResultsPage.java
@@ -1,6 +1,7 @@
 package edu.rit.csh;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -15,6 +16,7 @@ import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.datetime.StyleDateConverter;
 import org.apache.wicket.datetime.markup.html.form.DateTextField;
 import org.apache.wicket.extensions.yui.calendar.DatePicker;
+import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
@@ -100,7 +102,7 @@ public class OwnedBookSearchResultsPage extends PageTemplate {
 			DateTextField dateField = new DateTextField("date",
 					new PropertyModel<Date>(this, "date"),
 					new StyleDateConverter(false));
-			dateField.add(new DatePicker().setShowOnFieldClick(true).setAutoHide(true));
+			dateField.add(new ReturnDatePicker());
 			add(dateField);
 			setOutputMarkupId(true);
 		}
@@ -115,6 +117,40 @@ public class OwnedBookSearchResultsPage extends PageTemplate {
 			UserWebSession sess = (UserWebSession)this.getSession();
 			String uid = sess.getUser().getUidnumber();
 			b.borrow(uid, begin, end);
+		}
+	}
+	
+	private class ReturnDatePicker extends DatePicker{
+		private static final long serialVersionUID = -3239044315676274739L;
+		
+		public ReturnDatePicker(){
+			super();
+			setShowOnFieldClick(true);
+			setAutoHide(true);
+		}
+		
+		@Override
+		protected void configure(Map<String, Object> widgetProperties,
+							     IHeaderResponse response,
+							     Map<String, Object> initVariables){
+			/**
+			 * Set the minimum and maximum dates for the YUI date picker.
+			 */
+			SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+			
+			Calendar tomorrow = Calendar.getInstance();
+			Calendar aYearFromTomorrow = Calendar.getInstance();
+			
+			tomorrow.add(Calendar.DAY_OF_YEAR, 1);
+			aYearFromTomorrow.add(Calendar.DAY_OF_YEAR, 1);
+			aYearFromTomorrow.add(Calendar.YEAR, 1);
+			
+			String tmrwDate = sdf.format(tomorrow.getTime());
+			String ayftDate = sdf.format(aYearFromTomorrow.getTime());
+			widgetProperties.put("mindate", tmrwDate);
+			widgetProperties.put("maxdate", ayftDate);
+
+			super.configure(widgetProperties, response, initVariables);
 		}
 	}
 }
