30216f87ab90b468c636419a1b042c02604e9216
Scott Jordan <sleepinghippy@hotmail.com>
2014-04-01 16:04:56 -0400

    Improved the appearance of SearchBookPanel on RequestPage by giving it its own customized html template.

--- src/main/java/edu/rit/csh/components/SearchBookPanel.java
+++ src/main/java/edu/rit/csh/components/SearchBookPanel.java
@@ -6,11 +6,16 @@ public class SearchBookPanel extends Panel {
 	private static final long serialVersionUID = 1L;
 	
 	private final SearchBookForm form;
-
+    private String variant;
 	public SearchBookPanel(String id, String action) {
 		super(id);
+        variant = action;
 		form = new SearchBookForm("searchBookForm", action);
 		add(form);
 	}
 
+    @Override
+    public String getVariation(){
+        return variant;
+    }
 }
